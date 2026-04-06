6e4e211e165dcca2f2de100345fa4b9975a565d3
ShAhilK2 <80386008+ShAhilK2@users.noreply.github.com>
2026-04-06 10:33:38 +0530

    feat : implemented a Dashboard page UI

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  dummyAdminDashboardData,
+  dummyEmployeeDashboardData,
+} from "../assets/assets";
+import Loading from "../components/Loading";
+import EmployeeDashboard from "../components/Employee/EmployeeDashboard";
+import AdminDashboard from "../components/Admin/AdminDashboard";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setData(dummyAdminDashboardData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) return <Loading />;
+  if (!data)
+    return (
+      <p className="text-center text-slate-500">Failed to load dashboard</p>
+    );
+
+  if (data.role === "ADMIN") {
+    return <AdminDashboard data={data} />;
+  } else {
+    return <EmployeeDashboard data={data} />;
+  }
 };
 
 export default Dashboard;
